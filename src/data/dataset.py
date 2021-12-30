23447ab6cf45127025be63bb59e03ccf7dc69ac6
waverDeep <dandyleader@naver.com>
2021-12-30 21:05:54 +0900

    데이터셋을 로드할 수 있는 함수 구현

---
+++ src/data/dataset.py
@@ -0,0 +1,19 @@
+import src.data.dataset_k_food as k_food
+from torch.utils.data import Dataset, DataLoader
+
+def get_dataloader(config, mode='train'):
+    dataset = None
+    dataset_type = config['dataset_type']
+    if dataset_type == 'KFoodSampleDataset':
+        dataset = k_food.KFoodSampleDataset(
+            directory_path=config['{}_dataset'.format(mode)],
+        )
+
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=config['batch_size'],
+        shuffle=config['dataset_shuffle'],
+        num_workers=config['num_workers'],
+        pin_memory=config['pin_memory'],
+    )
+    return dataloader, dataset
