67fab3b50a774168dde497374acfc43f1b3f1794
waverDeep <dandyleader@naver.com>
2021-12-30 21:06:37 +0900

    인터페이스 구현: tensorboard

---
+++ src/utils/interface_tensorboard.py
@@ -0,0 +1,20 @@
+# 텐서보드를 사용해서 Projector를 구현할 때 오류가 있음
+# 이 오류를 해결하기 위해서 작성해야 할 것
+from torch.utils.tensorboard import SummaryWriter
+import tensorflow as tf
+import tensorboard as tb
+tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
+# console: tensorboard --logdir=runs --bind_all
+
+
+def set_tensorboard_writer(name):
+    writer = SummaryWriter(name) # 'runs/fashion_mnist_experiment_1'
+    return writer
+
+
+def inspect_model(writer, model, data):
+    writer.add_graph(model, data)
+
+
+def close_tensorboard_writer(writer):
+    writer.close()
