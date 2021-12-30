be540ca60c6a59d47a880ee4757acd85b60a2fdd
waverDeep <dandyleader@naver.com>
2021-12-30 21:07:40 +0900

    model: model load관련 함수 구현

--- src/models/model.py
+++ src/models/model.py
@@ -1,12 +1,19 @@
 import src.models.model_baseline as model_baseline
+import torch
 
-def load_model(config):
+
+def load_model(config, checkpoint_path=None):
     model_name = config['model_name']
     model = None
     if model_name == 'VGG16Combine':
-        pass
+        model = model_baseline.VGG16Combine(
+            last_node=config['last_node'],
+            num_classes=config['num_classes'],
+        )
 
     if config['model_checkpoint'] is not None:
-        pass
+        device = torch.device('cpu')
+        checkpoint = torch.load(checkpoint_path, map_location=device)
+        model.load_state_dict(checkpoint['model_state_dict'])
 
     return model
