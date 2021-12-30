ee8fb81429e98c260c3a848784da027f3df830ae
waverDeep <dandyleader@naver.com>
2021-12-30 21:08:01 +0900

    model: 추후에 특정 layer만 추출할 수 있도록 구현

--- src/models/model_baseline.py
+++ src/models/model_baseline.py
@@ -7,10 +7,16 @@ import torchvision.models as vision_model
 class VGG16Combine(nn.Module):
     def __init__(self, last_node=131072, num_classes=10):
         super(VGG16Combine, self).__init__()
-        self.network = nn.Sequential(
+        self.feature_extract_network = nn.Sequential(
             collections.OrderedDict(
                 [
                     ("feature_extract_layer", vision_model.vgg16_bn(pretrained=True).features),
+                ]
+            )
+        )
+        self.post_network = nn.Sequential(
+            collections.OrderedDict(
+                [
                     ("flatten_layer", nn.Flatten()),
                     ("post_layer", nn.Linear(last_node, num_classes)),
                 ]
@@ -18,7 +24,8 @@ class VGG16Combine(nn.Module):
         )
 
     def forward(self, x):
-        out = self.network(x)
+        out = self.feature_extract_network(x)
+        out = self.post_network(out)
         return out
 
 
@@ -27,4 +34,7 @@ if __name__ == '__main__':
     sample_data = torch.randn(8, 3, 512, 512)
     sample_out = test_model(sample_data)
 
-    print(sample_out.size())
+    print(sample_out)
+
+    _, predicted = torch.max(sample_out.data, 1)
+    print(predicted)
