458f1a9b9591692eff97207cc7c6bc8a878ea7f2
waverDeep <dandyleader@naver.com>
2021-12-31 14:12:32 +0900

    tensorboard: 텐서보드에서 모델의 구조와 데이터셋 sample을 확인해볼 수 있도록 하는 코드 추가

--- src/utils/interface_tensorboard.py
+++ src/utils/interface_tensorboard.py
@@ -3,8 +3,24 @@
 from torch.utils.tensorboard import SummaryWriter
 import tensorflow as tf
 import tensorboard as tb
+import torchvision
+import matplotlib.pyplot as plt
+import numpy as np
 tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
 # console: tensorboard --logdir=runs --bind_all
+
+
+# helper function to show an image
+# (used in the `plot_classes_preds` function below)
+def matplotlib_imshow(img, one_channel=False):
+    if one_channel:
+        img = img.mean(dim=0)
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    if one_channel:
+        plt.imshow(npimg, cmap="Greys")
+    else:
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
 
 def set_tensorboard_writer(name):
@@ -18,3 +34,11 @@ def inspect_model(writer, model, data):
 
 def close_tensorboard_writer(writer):
     writer.close()
+
+
+def add_image_on_tensorboard(writer, dataloader, desc="dataset"):
+    tdataiter = iter(dataloader)
+    images, labels, detail_labels = tdataiter.next()
+    img_grid = torchvision.utils.make_grid(images)
+    writer.add_image('{}/images of {}'.format(desc, desc), img_grid)
+
