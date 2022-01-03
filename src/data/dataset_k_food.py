4e4fe90dc0ebab68337d27c3d10cef1640025c05
waverDeep <dandyleader@naver.com>
2022-01-03 12:27:40 +0900

    dataset: transform 관련 코드 수정

--- src/data/dataset_k_food.py
+++ src/data/dataset_k_food.py
@@ -36,25 +36,30 @@ class KFoodSampleDataset(Dataset):
 # num_classes = 150 (detail)
 # num_classes = 27
 class KFoodDataset(Dataset):
-    def __init__(self, directory_path, mode='train'):
+    def __init__(self, directory_path, mode='train', crop_size=512):
         super(KFoodDataset, self).__init__()
         self.detail_label_list = file_io.read_txt2list('./dataset/kfood-category-detail.txt')
         self.label_list = file_io.read_txt2list('./dataset/kfood-category.txt')
         self.file_list = file_io.read_txt2list(directory_path)
         if mode == 'train':
-            self.image_transforms = nn.Sequential(
-                transforms.ToTensor(),
-                transforms.RandomResizedCrop(512),
-                # transforms.RandomHorizontalFlip(),
-                # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-                # transforms.RandomRotation(degrees=(0, 360)),
+            self.image_transforms = transforms.Compose(
+                [
+                    transforms.RandomResizedCrop(crop_size),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.RandomRotation(degrees=(0, 360)),
+                    transforms.ToTensor(),
+                    # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
 
+                ]
             )
         else:
-            self.image_transforms = nn.Sequential(
-                # transforms.Resize(640),
-                transforms.CenterCrop(512),
-                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+            self.image_transforms = transforms.Compose(
+                [
+                    transforms.Resize(640),
+                    transforms.CenterCrop(crop_size),
+                    transforms.ToTensor(),
+                    # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+                ]
             )
 
     def __len__(self):
@@ -63,7 +68,6 @@ class KFoodDataset(Dataset):
     def __getitem__(self, x):
         # './dataset/kfood/구이/갈비구이/Img_000_0000.jpg'
         file = self.file_list[x]
-        print(file)
         label = self.label_list.index(file.split('/')[3])
         detail_label = self.detail_label_list.index(file.split('/')[4])
         # have issue - png 4channel cannot load only supported 3 dimension
