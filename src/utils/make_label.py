c99f7d9edb9872a3c0a39fb8d94b45f3d92cc7c3
waverDeep <dandyleader@naver.com>
2022-02-26 16:17:13 +0900

    dataset: make private dataset label

--- src/utils/make_label.py
+++ src/utils/make_label.py
@@ -4,10 +4,33 @@ import natsort
 import interface_file_io as file_io
 
 
-def get_directory_to_label(root_dir):
-    dir_list = natsort.natsorted(os.listdir(root_dir))
-    file_io.make_list2txt(dir_list, '../../dataset/FruitandVegsLabel.txt')
+def get_directory_to_label(dataset_dir):
+    data_list = natsort.natsorted(os.listdir(dataset_dir))
+    for idx, data in enumerate(data_list):
+        data = data.lower()
+        data_list[idx] = data.replace(' ', '_')
+    return data_list
+
+
+def get_label(dataset_dir_list, output_filepath):
+    data_list = []
+    for dataset_dir in dataset_dir_list:
+        data_list += get_directory_to_label(dataset_dir)
+    data_list = natsort.natsorted(list(set(data_list)))
+    file_io.make_list2txt(data_list, output_filepath)
+
+
+def get_label(dataset_dir, output_file):
+    label_list = os.listdir(dataset_dir)
+    label_list = list(set(label_list))
+    file_io.make_list2txt(label_list, output_file)
 
 
 if __name__ == '__main__':
-    get_directory_to_label('../../dataset/FruitandVegs/train')
+    dir_list = [
+        '../../dataset/FruitsandVegs/train',
+        '../../dataset/VegetableImages/train'
+    ]
+
+    output = '../../dataset/FV-label.txt'
+    get_label(dir_list, output)
